514a29f33d3d44667f2409ef4f43e41a1d9999fa
MahamCode <maham.work121@gmail.com>
2022-08-26 15:00:35 +0500

    doc

--- Phrases/src/greetings/english.rs
+++ Phrases/src/greetings/english.rs
@@ -1,3 +1,16 @@
+//! This module contains english phrases
+//!
+//! # Examples
+//! ```
+
+//!  //Now here whatever I write will be completely valid or compilable code
+//! let username = "maham";
+//! Println!(“{}, {}”, phrases::greetings::english::hello(), username);
+
+//!```
+//! to make it's doc run the command below:
+//! rustdoc english.rs
+
 pub fn hello() -> String {
     "hello".to_string()
 }
